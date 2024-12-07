65aee75b23e3cf80f3d884487c4b604477162e40
Josh Odegai <JoshOdegai@my.unt.edu>
2024-12-07 00:36:30 -0600

    Further adjustments

--- app/src/main/java/com/example/dynamicviews/MainActivity.kt
+++ app/src/main/java/com/example/dynamicviews/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.dynamicviews
 
+import android.app.Activity
 import android.content.Intent
+import android.app.ActivityOptions
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -10,6 +13,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,16 +23,19 @@ class MainActivity : AppCompatActivity() {
         strawBtn.setOnClickListener(View.OnClickListener {
             val intent = Intent(this,Strawberry::class.java)
             startActivity(intent)
+            applyTransition()
         })
         val oranBtn = findViewById<Button>(R.id.buttonOrange)
         oranBtn.setOnClickListener(View.OnClickListener {
             val intent = Intent(this,Orange::class.java)
             startActivity(intent)
+            applyTransition()
         })
         val banaBtn = findViewById<Button>(R.id.buttonBanana)
         banaBtn.setOnClickListener(View.OnClickListener {
             val intent = Intent(this,Banana::class.java)
             startActivity(intent)
+            applyTransition()
         })
 
 
@@ -36,6 +43,16 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+            }
+    private fun applyTransition() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            this.overrideActivityTransition(
+                Activity.OVERRIDE_TRANSITION_OPEN,
+                R.anim.animate_fade_enter, // Enter animation
+                R.anim.animate_fade_exit  // Exit animation
+            )
         }
     }
 }
